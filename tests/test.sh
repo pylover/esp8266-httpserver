a499d0daeeb529f171dada3203a100af1ba1250f
pylover <vahid.mardani@gmail.com>
2020-09-15 23:50:31 +0430

    Make file for tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-make -f _Makefile clean 
-make -f _Makefile all
+make -f Makefile clean 
+make -f Makefile all
 
 if [ -z "$1" ]; then
 	echo "Build Done"
 elif [ $1 == "d" ]; then
 	gdb $2
 elif [ $1 == "a" ]; then
-	make -f _Makefile test
+	make -f Makefile test
 else
 	./$1
 fi
